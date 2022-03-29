7863524dc8ea0e6c67b11ecc8901368a1e37eabe
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-03-30 00:04:18 +0200

    feat: add year control

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/academic_course/models/academic_course.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/academic_course/models/academic_course.py
@@ -1,8 +1,9 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class AcademicCourse(models.Model):
-    initialYear = models.IntegerField(unique=True)
+    initialYear = models.IntegerField(unique=True, validators=[MinValueValidator(1000), MaxValueValidator(3000)])
 
     def __str__(self) -> str:
         return f'{str(self.initialYear)}-{str(self.initialYear + 1)}'
--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/test_child.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/test_child.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from model_bakery import baker
 
@@ -8,3 +9,17 @@ class TestChild(TestCase):
     def test_str(self):
         child: Child = baker.make('Child')
         self.assertEqual(str(child), "{} {}".format(child.name, str(child.family)))
+
+    def test_year_lower_minimal(self):
+        with self.assertRaises(ValidationError):
+            child: Child = Child(name='name_min', year_of_birth=999, family=baker.make('Family'))
+            child.full_clean()
+
+    def test_year_major_maximum(self):
+        with self.assertRaises(ValidationError):
+            child: Child = Child(name='name_min', year_of_birth=3001, family=baker.make('Family'))
+            child.full_clean()
+
+    def test_year_on_range(self):
+        child: Child = Child(name='name_min', year_of_birth=2022, family=baker.make('Family'))
+        child.full_clean()
