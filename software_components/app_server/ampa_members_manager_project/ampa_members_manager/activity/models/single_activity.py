f166be9d4c7d85a47b9491ee3c67ca3fc748fb3a
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-04-01 23:40:21 +0200

    feat: add price to single_activities

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/activity/models/single_activity.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/activity/models/single_activity.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
+class PaymentType(models.IntegerChoices):
+    SINGLE = 1
+    PER_DAY = 2
+
+
 class SingleActivity(models.Model):
     name = models.CharField(max_length=100)
+    price_for_member = models.DecimalField(max_digits=6, decimal_places=2)
+    price_for_no_member = models.DecimalField(max_digits=6, decimal_places=2)
+    payment_type = models.IntegerField(choices=PaymentType.choices)
 
     def __str__(self) -> str:
         return f'{self.name}'
