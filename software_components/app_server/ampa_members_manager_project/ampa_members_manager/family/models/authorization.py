e85e2236ef465c064ff411253fd02206cece9c12
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-05-15 00:49:04 +0200

    feat(authorization): add number and year constraint

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/authorization.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/authorization.py
@@ -7,8 +7,12 @@ from ampa_members_manager.family.models.bank_account import BankAccount
 
 class Authorization(models.Model):
     number = models.CharField(max_length=50)
-    year = models.IntegerField(unique=True, validators=[MinValueValidator(1000), MaxValueValidator(3000)])
+    year = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(3000)])
     bank_account = models.OneToOneField(to=BankAccount, on_delete=CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['number', 'year'], name='unique_number_in_a_year')]
 
     def __str__(self) -> str:
         return f'{self.year}/{self.number}-{str(self.bank_account)}'
