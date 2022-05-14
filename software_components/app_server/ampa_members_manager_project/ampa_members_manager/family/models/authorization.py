79a3dc0abaef7ad0437c9cbd1b091b8aa5bb11c9
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-05-15 00:43:42 +0200

    feat(authorization): change admin name

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/authorization.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/authorization.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models import CASCADE
 
@@ -6,8 +7,8 @@ from ampa_members_manager.family.models.bank_account import BankAccount
 
 class Authorization(models.Model):
     number = models.CharField(max_length=50)
-    date = models.DateField()
+    year = models.IntegerField(unique=True, validators=[MinValueValidator(1000), MaxValueValidator(3000)])
     bank_account = models.OneToOneField(to=BankAccount, on_delete=CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.number}-{str(self.bank_account)}'
+        return f'{self.year}/{self.number}-{str(self.bank_account)}'
