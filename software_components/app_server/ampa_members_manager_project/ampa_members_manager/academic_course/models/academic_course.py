14ed5203e06e8791909cc9379db3b23b8b5aca1b
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-05-18 22:59:09 +0200

    feat(AcademicCourse): rename initialYear to initial_year

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/academic_course/models/academic_course.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/academic_course/models/academic_course.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class AcademicCourse(models.Model):
-    initialYear = models.IntegerField('Initial year', unique=True, validators=[MinValueValidator(1000), MaxValueValidator(3000)])
+    initial_year = models.IntegerField('Initial year', unique=True, validators=[MinValueValidator(1000), MaxValueValidator(3000)])
     fee = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self) -> str:
-        return f'{str(self.initialYear)}-{str(self.initialYear + 1)}'
+        return f'{str(self.initial_year)}-{str(self.initial_year + 1)}'
--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/academic_course/models/test_academic_course.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/academic_course/models/test_academic_course.py
@@ -7,19 +7,19 @@ from ampa_members_manager.academic_course.models.academic_course import Academic
 
 class TestAcademicCourse(TestCase):
     def test_str(self):
-        academic_course = AcademicCourse(initialYear=2022)
+        academic_course = AcademicCourse(initial_year=2022)
         self.assertEqual(str(academic_course), "2022-2023")
 
     def test_year_lower_minimal(self):
         with self.assertRaises(ValidationError):
-            child: AcademicCourse = AcademicCourse(initialYear=999)
+            child: AcademicCourse = AcademicCourse(initial_year=999)
             child.full_clean()
 
     def test_year_major_maximum(self):
         with self.assertRaises(ValidationError):
-            child: AcademicCourse = AcademicCourse(initialYear=3001)
+            child: AcademicCourse = AcademicCourse(initial_year=3001)
             child.full_clean()
 
     def test_year_on_range(self):
-        child: AcademicCourse = AcademicCourse(initialYear=2022)
+        child: AcademicCourse = AcademicCourse(initial_year=2022)
         child.full_clean()
