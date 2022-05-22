f2b540ed1741231d14d11aa004fda94f6810e11e
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-05-23 00:17:44 +0200

    feat(Charge): start with create_filled_charge_group

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/charge/models/charge_group.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/charge/models/charge_group.py
@@ -1,8 +1,24 @@
-from django.db import models
+from __future__ import annotations
+from django.db import models, transaction
+from django.db.models import QuerySet
 
 from ampa_members_manager.activity.models.single_activity import SingleActivity
+
+
+class NoSingleActivityError(Exception):
+    def __init__(self):
+        super().__init__("NoSingleActivityError")
 
 
 class ChargeGroup(models.Model):
     single_activities = models.ManyToManyField(to=SingleActivity)
 
+    @classmethod
+    def create_filled_charge_group(cls, single_activities: QuerySet[SingleActivity]) -> ChargeGroup:
+        if not single_activities.exists():
+            raise NoSingleActivityError
+
+        with transaction.atomic():
+            charge_group: ChargeGroup = ChargeGroup.objects.create()
+            charge_group.single_activities.set(single_activities)
+            return charge_group
---
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/charge/models/test_charge_group.py
@@ -0,0 +1,20 @@
+from django.db.models import QuerySet
+from django.test import TestCase
+from model_bakery import baker
+
+from ampa_members_manager.activity.models.single_activity import SingleActivity
+from ampa_members_manager.charge.models.charge_group import ChargeGroup, NoSingleActivityError
+
+
+class TestChargeGroup(TestCase):
+    def test_create_filled_charge_group_no_single_activity(self):
+        single_activities_none: QuerySet[SingleActivity] = SingleActivity.objects.none()
+        with self.assertRaises(NoSingleActivityError):
+            ChargeGroup.create_filled_charge_group(single_activities_none)
+
+    def test_create_filled_charge_group_one_single_activity_one_activity_registration(self):
+        single_activity: SingleActivity = baker.make('SingleActivity')
+        single_activities_one: QuerySet[SingleActivity] = SingleActivity.objects.all()
+        charge_group: ChargeGroup = ChargeGroup.create_filled_charge_group(single_activities_one)
+        self.assertIsNotNone(charge_group.single_activities)
+        self.assertEqual(list(charge_group.single_activities.all()), list([single_activity]))
