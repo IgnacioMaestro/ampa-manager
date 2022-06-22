fbf76e773f6fd82d9475631598ceeb63637ee67f
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-06-23 00:46:35 +0200

    refactor: protect

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/charge/use_cases/create_charge_group_with_charges/charges_creator.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/charge/use_cases/create_charge_group_with_charges/charges_creator.py
@@ -7,11 +7,11 @@ from ampa_members_manager.charge.models.charge_group import ChargeGroup
 
 class ChargesCreator:
     def __init__(self, charge_group: ChargeGroup):
-        self.charge_group: ChargeGroup = charge_group
+        self.__charge_group: ChargeGroup = charge_group
 
     def create(self):
         activity_registrations: List[ActivityRegistration] = []
-        for single_activity in self.charge_group.single_activities.all():
+        for single_activity in self.__charge_group.single_activities.all():
             activity_registrations.extend(ActivityRegistration.with_single_activity(single_activity=single_activity))
         for activity_registration in activity_registrations:
             charge: Charge
@@ -20,6 +20,6 @@ class ChargesCreator:
             except NotFound:
                 price: float = activity_registration.single_activity.calculate_price(
                     times=activity_registration.amount, membership=activity_registration.is_membership())
-                charge: Charge = Charge.objects.create(group=self.charge_group, amount=price)
+                charge: Charge = Charge.objects.create(group=self.__charge_group, amount=price)
             charge.activity_registrations.add(activity_registration)
             charge.save()
