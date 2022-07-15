e3ce1c14c21e511a6a8f809c9e1efcd7afa3aee7
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-07-15 13:44:05 +0200

    feat: check all single activities belongs to same repetitive activity

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/activity/admin.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/activity/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
 
+from ampa_members_manager.activity.models.repetitive_activity import RepetitiveActivity
 from ampa_members_manager.activity.models.single_activity import SingleActivity
 from ampa_members_manager.charge.use_cases.create_charge_group_with_charges.charge_group_with_charges_creator import \
     ChargeGroupWithChargesCreator
@@ -17,8 +18,19 @@ class UniqueActivityAdmin(admin.ModelAdmin):
 
 class SingleActivityAdmin(admin.ModelAdmin):
     @admin.action(description=_("Create charge group"))
-    def create_charge_group(self, request, queryset: QuerySet[SingleActivity]):
-        ChargeGroupWithChargesCreator(queryset).create()
+    def create_charge_group(self, request, single_activities: QuerySet[SingleActivity]):
+        if not self.all_same_repetitive_activity(single_activities=single_activities):
+            message = _("All Single Activities must be from the same repetitive activity")
+            return self.message_user(request, message)
+        ChargeGroupWithChargesCreator(single_activities).create()
         return self.message_user(request=request, message=_("Charge group created"))
+
+    def all_same_repetitive_activity(self, single_activities: QuerySet[SingleActivity]) -> bool:
+        first_single_activity: SingleActivity = single_activities.first()
+        repetitive_activity: RepetitiveActivity = first_single_activity.repetitiveactivity_set.first()
+        for single_activity in single_activities.all():
+            if single_activity.repetitiveactivity_set.first() != repetitive_activity:
+                return False
+        return True
 
     actions = [create_charge_group]
