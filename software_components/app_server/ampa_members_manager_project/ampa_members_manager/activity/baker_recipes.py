1e9699cfcd4f93e1b0a2352f4f7d8d4891b60951
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-07-21 14:54:04 +0200

    refactor: divide recipes

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/activity/baker_recipes.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/activity/baker_recipes.py
@@ -1,5 +1,18 @@
 from model_bakery.recipe import Recipe, foreign_key
 
+from ampa_members_manager.activity.models.repetitive_activity import RepetitiveActivity
+from ampa_members_manager.activity.models.single_activity import SingleActivity
 from ampa_members_manager.activity.models.unique_activity import UniqueActivity
 from ampa_members_manager.activity_registration.models.activity_registration import ActivityRegistration
-from ampa_members_manager.baker_recipes import single_activity_with_unique_activity
+
+unique_activity_local_recipe = Recipe(UniqueActivity)
+repetitive_activity_local_recipe = Recipe(RepetitiveActivity)
+
+single_activity_with_unique_activity_local_recipe = Recipe(
+    SingleActivity, unique_activity=foreign_key(unique_activity_local_recipe))
+
+single_activity_with_repetitive_activity_local_recipe = Recipe(
+    SingleActivity, repetitive_activity=foreign_key(repetitive_activity_local_recipe))
+
+activity_registration_with_single_activity_local_recipe = Recipe(
+    ActivityRegistration, single_activity=foreign_key(single_activity_with_unique_activity_local_recipe))
--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/baker_recipes.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/baker_recipes.py
@@ -1,20 +1,13 @@
-from model_bakery.recipe import Recipe, foreign_key
+from ampa_members_manager.activity.baker_recipes import unique_activity_local_recipe, repetitive_activity_local_recipe, \
+    single_activity_with_unique_activity_local_recipe, single_activity_with_repetitive_activity_local_recipe, \
+    activity_registration_with_single_activity_local_recipe
 
-from ampa_members_manager.activity.models.repetitive_activity import RepetitiveActivity
-from ampa_members_manager.activity.models.single_activity import SingleActivity
-from ampa_members_manager.activity.models.unique_activity import UniqueActivity
-from ampa_members_manager.activity_registration.models.activity_registration import ActivityRegistration
+unique_activity = unique_activity_local_recipe
+repetitive_activity = repetitive_activity_local_recipe
+single_activity_with_unique_activity_recipe = single_activity_with_unique_activity_local_recipe
+single_activity_with_repetitive_activity_recipe = single_activity_with_repetitive_activity_local_recipe
+activity_registration_with_single_activity_recipe = activity_registration_with_single_activity_local_recipe
 
-unique_activity = Recipe(UniqueActivity)
-repetitive_activity = Recipe(RepetitiveActivity)
-
-__single_activity_with_unique_activity = Recipe(SingleActivity, unique_activity=foreign_key(unique_activity))
-single_activity_with_unique_activity = 'ampa_members_manager.__single_activity_with_unique_activity'
-
-__single_activity_with_repetitive_activity = Recipe(SingleActivity,
-                                                    repetitive_activity=foreign_key(repetitive_activity))
-single_activity_with_repetitive_activity = 'ampa_members_manager.__single_activity_with_repetitive_activity'
-
-__activity_registration_with_single_activity = Recipe(
-    ActivityRegistration, single_activity=foreign_key(single_activity_with_unique_activity))
-activity_registration_with_single_activity = 'ampa_members_manager.__activity_registration_with_single_activity'
+single_activity_with_unique_activity = 'ampa_members_manager.single_activity_with_unique_activity_recipe'
+single_activity_with_repetitive_activity = 'ampa_members_manager.single_activity_with_repetitive_activity_recipe'
+activity_registration_with_single_activity = 'ampa_members_manager.activity_registration_with_single_activity_recipe'
