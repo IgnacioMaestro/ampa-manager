f3d21fa2a6aed5d27e19fbde488963990c3ed1b7
Daniel Landa <daniel.landa@tecnalia.com>
2022-08-25 13:13:44 +0200

    auth state corrected

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/state.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/state.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class State(models.IntegerChoices):
     NOT_SENT = (1, _('Not sent'))
     SENT = (2, _('Sent'))
-    SIGNED = (3, _('No Signed'))
+    SIGNED = (3, _('Signed'))
