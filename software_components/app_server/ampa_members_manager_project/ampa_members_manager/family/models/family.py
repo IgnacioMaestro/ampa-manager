5fc03218f1de3e23a11103650ade8d889f5a31ef
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-08-25 09:48:22 +0200

    feat: join two surnames

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/family.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/family.py
@@ -8,20 +8,20 @@ from ampa_members_manager.family.models.parent import Parent
 
 
 class Family(models.Model):
-    first_surname = models.CharField(max_length=500, verbose_name=_("First surname"))
-    second_surname = models.CharField(max_length=500, verbose_name=_("Second surname"))
+    surnames = models.CharField(max_length=500, verbose_name=_("Surnames"))
     email = models.EmailField(unique=True, verbose_name=_("Email"))
     secondary_email = models.EmailField(null=True, blank=True, verbose_name=_("Secondary Email"))
     parents = models.ManyToManyField(to=Parent, verbose_name=_("Parents"))
     default_bank_account = models.ForeignKey(
-        to=BankAccount, on_delete=SET_NULL, null=True, blank=True, verbose_name=_("Default bank account"), help_text=_("Save the family to see its bank accounts"))
+        to=BankAccount, on_delete=SET_NULL, null=True, blank=True, verbose_name=_("Default bank account"),
+        help_text=_("Save the family to see its bank accounts"))
 
     class Meta:
         verbose_name = _('Family')
         verbose_name_plural = _("Families")
 
     def __str__(self) -> str:
-        return f'{self.first_surname} {self.second_surname}'
+        return f'{self.surnames}'
 
     @classmethod
     def all_families(cls) -> QuerySet[Family]:
