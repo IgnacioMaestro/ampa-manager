0ab7575fca5cb6f271f602480299995d913d02a3
Daniel Landa <daniel.landa@tecnalia.com>
2022-09-29 10:25:58 +0200

    new inline activity receipts for family

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/read_only_inline.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/read_only_inline.py
@@ -1,7 +1,18 @@
-from django.contrib.admin import TabularInline
+from django.contrib.admin import TabularInline, StackedInline
 
 
 class ReadOnlyTabularInline(TabularInline):
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class ReadOnlyStackedInline(StackedInline):
     def has_change_permission(self, request, obj=None):
         return False
 
