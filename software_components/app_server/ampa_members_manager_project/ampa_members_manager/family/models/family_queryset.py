9fe65ef2ed7682d80536df7a2dc1ec2224e3af16
Daniel Landa <daniel.landa@tecnalia.com>
2022-10-14 09:58:12 +0200

    get_member_families_ids renamed

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/family_queryset.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager/family/models/family_queryset.py
@@ -10,12 +10,12 @@ from ampa_members_manager.academic_course.models.course_name import CourseName
 class FamilyQuerySet(QuerySet):
 
     def has_any_children(self):
-        return self.filter(id__in=FamilyQuerySet.get_member_families_ids())
+        return self.filter(id__in=FamilyQuerySet.get_families_with_school_children_ids())
     
     def has_no_children(self):
-        return self.exclude(id__in=FamilyQuerySet.get_member_families_ids())
+        return self.exclude(id__in=FamilyQuerySet.get_families_with_school_children_ids())
     
-    def get_member_families_ids():
+    def get_families_with_school_children_ids():
         active_course = ActiveCourse.load()
         childs_with_age = Child.objects.annotate(age=active_course.initial_year - F('year_of_birth') - F('repetition'))
         childs_distinct_families = childs_with_age.filter(age__range=(CourseName.AGE_HH2, CourseName.AGE_LH6))
