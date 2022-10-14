8fbbb53120aa275f93525b5d1ddfeef23fda3af1
Daniel Landa <daniel.landa@tecnalia.com>
2022-10-14 14:10:26 +0200

    favicon

--- software_components/app_server/ampa_members_manager_project/ampa_members_manager_project/urls.py
+++ software_components/app_server/ampa_members_manager_project/ampa_members_manager_project/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
