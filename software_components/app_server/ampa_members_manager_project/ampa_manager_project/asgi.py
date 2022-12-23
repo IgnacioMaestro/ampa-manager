ccac2567b64f4834e444cc24189f7403b069502d
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-12-23 16:37:04 +0100

    feat: rename project ampa_members_manager_project to ampa_manager_project

--- software_components/app_server/ampa_members_manager_project/ampa_manager_project/asgi.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ampa_members_manager_project project.
+ASGI config for ampa_manager_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ampa_members_manager_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ampa_manager_project.settings')
 
 application = get_asgi_application()
--- software_components/app_server/ampa_members_manager_project/ampa_manager_project/urls.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager_project/urls.py
@@ -1,4 +1,4 @@
-"""ampa_members_manager_project URL Configuration
+"""ampa_manager_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
