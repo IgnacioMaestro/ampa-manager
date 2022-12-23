cc213d3518b82d81e4c76065581630fb33ad247d
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2022-12-23 16:37:04 +0100

    feat: rename project ampa_members_manager_project to ampa_manager_project

--- software_components/app_server/ampa_members_manager_project/ampa_manager_project/wsgi.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ampa_members_manager_project project.
+WSGI config for ampa_manager_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ampa_members_manager_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ampa_manager_project.settings')
 
 application = get_wsgi_application()
