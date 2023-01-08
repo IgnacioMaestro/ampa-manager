1e894aef0f773ae8c9d04cb1a7e5fca89c7ae95c
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-01-08 09:25:05 +0100

    refactor: improve names

--- software_components/app_server/ampa_members_manager_project/ampa_manager/charge/use_cases/after_school/test_remittance_generator_from_after_school_remittance.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/charge/use_cases/after_school/test_remittance_generator_from_after_school_remittance.py
@@ -5,20 +5,20 @@ from ampa_manager.academic_course.models.active_course import ActiveCourse
 from ampa_manager.charge.remittance import Remittance
 from ampa_manager.models import AfterSchoolRemittance
 from ampa_manager.tests.generator_adder import bic_generator, phonenumbers_generator, iban_generator
-from .after_school_remittance_generator import AfterSchoolRemittanceGenerator
+from .remittance_generator_from_after_school_remittance import RemittanceGeneratorFromAfterSchoolRemittance
 
 baker.generators.add('localflavor.generic.models.BICField', bic_generator)
 baker.generators.add('phonenumber_field.modelfields.PhoneNumberField', phonenumbers_generator)
 baker.generators.add('localflavor.generic.models.IBANField', iban_generator)
 
 
-class TestAfterSchoolGenerator(TestCase):
+class TestRemittanceGeneratorFromAfterSchoolRemittance(TestCase):
     def test_generate_after_school_remittance_no_receipts(self):
         # Arrange
         after_school_remittance: AfterSchoolRemittance = baker.make('AfterSchoolRemittance')
 
         # Act
-        remittance: Remittance = AfterSchoolRemittanceGenerator(
+        remittance: Remittance = RemittanceGeneratorFromAfterSchoolRemittance(
             after_school_remittance=after_school_remittance).generate()
 
         # Assert
@@ -32,7 +32,7 @@ class TestAfterSchoolGenerator(TestCase):
         baker.make('AfterSchoolReceipt', remittance=after_school_remittance)
 
         # Act
-        remittance: Remittance = AfterSchoolRemittanceGenerator(
+        remittance: Remittance = RemittanceGeneratorFromAfterSchoolRemittance(
             after_school_remittance=after_school_remittance).generate()
 
         # Assert
