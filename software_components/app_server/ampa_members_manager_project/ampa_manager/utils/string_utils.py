ee5884796cbf1402b8c665780026d82edf5c7e38
Daniel Landa <daniel.landa@tecnalia.com>
2023-01-16 13:59:30 +0100

    exec commands from admin

--- software_components/app_server/ampa_members_manager_project/ampa_manager/utils/string_utils.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/utils/string_utils.py
@@ -112,3 +112,50 @@ class StringUtils:
             return value.casefold()
         else:
             return None
+
+    @staticmethod
+    def get_excel_column_letter(column_index: int):
+        letters = {
+            0: 'A',
+            1: 'B',
+            2: 'C',
+            3: 'D',
+            4: 'E',
+            5: 'F',
+            6: 'G',
+            7: 'H',
+            8: 'I',
+            9: 'J',
+            10: 'K',
+            11: 'L',
+            12: 'M',
+            13: 'N',
+            14: 'O',
+            15: 'P',
+            16: 'Q',
+            17: 'R',
+            18: 'S',
+            19: 'T',
+            20: 'U',
+            21: 'V',
+            22: 'W',
+            23: 'X',
+            24: 'Y',
+            25: 'Z',
+            26: 'AA',
+            27: 'AB',
+            28: 'AC',
+            29: 'AD',
+            30: 'AE',
+            31: 'AF',
+            32: 'AG',
+            33: 'AH',
+            34: 'AI',
+            35: 'AJ',
+            36: 'AK',
+            37: 'AL',
+            38: 'AM',
+            39: 'AN',
+            40: 'AO',
+        }
+        return letters.get(column_index, '-')
