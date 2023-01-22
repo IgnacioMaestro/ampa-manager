0903df4fad76971c749004b74a52aea4ddd7cd08
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-01-22 17:24:31 +0100

    feat: command for migration

---
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/management/commands/migrate_to_holder.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from ampa_manager.management.commands.to_holder_migrator.to_holder_migrator import ToHolderMigrator
+
+
+class Command(BaseCommand):
+    help = "Migration to Holder data model."
+
+    def handle(self, *args, **options):
+        ToHolderMigrator.migrate()
