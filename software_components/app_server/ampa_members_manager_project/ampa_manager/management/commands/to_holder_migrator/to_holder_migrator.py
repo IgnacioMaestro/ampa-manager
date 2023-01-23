d90705e3884559786aa5928d70a8f62ee91f3841
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-01-23 14:31:29 +0100

    feat: migrate process

--- software_components/app_server/ampa_members_manager_project/ampa_manager/management/commands/to_holder_migrator/to_holder_migrator.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/management/commands/to_holder_migrator/to_holder_migrator.py
@@ -1,3 +1,4 @@
+from ampa_manager.activity.models.after_school.after_school_registration import AfterSchoolRegistration
 from ampa_manager.family.models.authorization.authorization_old import AuthorizationOld
 from ampa_manager.family.models.bank_account.bank_account import BankAccount
 from ampa_manager.family.models.family import Family
@@ -19,3 +20,16 @@ class ToHolderMigrator:
                 family.save()
             except Family.DoesNotExist:
                 pass
+
+    @classmethod
+    def migrate_after_school_registrations(cls):
+        after_school_registration: AfterSchoolRegistration
+        for after_school_registration in AfterSchoolRegistration.objects.all().iterator():
+            try:
+                holder: Holder = Holder.objects.filter(bank_account=after_school_registration.bank_account).get()
+                after_school_registration.holder = holder
+                after_school_registration.save()
+            except Holder.DoesNotExist:
+                print('error no holder for that bank account')
+            except Holder.MultipleObjectsReturned:
+                print('error bank account with two holders')
