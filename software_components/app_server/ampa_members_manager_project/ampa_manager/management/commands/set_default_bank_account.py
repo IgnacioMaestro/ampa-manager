7bbf6b227d1422b2cbc258dce793a17674ab99c1
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-01-24 02:07:30 +0100

    feat: remove default_bank_account from family

--- software_components/app_server/ampa_members_manager_project/ampa_manager/management/commands/set_default_bank_account.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/management/commands/set_default_bank_account.py
@@ -1,18 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.db.models import QuerySet
 
-from ampa_manager.family.models.bank_account.bank_account import BankAccount
 from ampa_manager.family.models.family import Family
+from ampa_manager.family.models.holder.holder import Holder
 
 
 class Command(BaseCommand):
     help = 'Set default bank account for families with only 1 bank account'
 
     def handle(self, *args, **options):
-        for family in Family.objects.without_default_bank_account():
-            bank_accounts: QuerySet[BankAccount] = BankAccount.objects.of_family(family)
-            if (bank_accounts.count()) == 1:
-                family.default_bank_account = bank_accounts.first()
+        for family in Family.objects.without_default_holder():
+            holders: QuerySet[Holder] = Holder.objects.of_family(family)
+            if (holders.count()) == 1:
+                family.default_holder = holders.first()
                 family.save()
             else:
-                print(f'Family {family}: not set. Accounts: {bank_accounts.count()}')
+                print(f'Family {family}: not set. Accounts: {holders.count()}')
