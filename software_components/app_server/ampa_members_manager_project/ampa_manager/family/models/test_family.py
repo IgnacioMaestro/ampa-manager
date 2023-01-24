90dd7433f05a17aac15ac11a0543fe7d11532540
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-01-24 09:47:37 +0100

    refactor: improve test

--- software_components/app_server/ampa_members_manager_project/ampa_manager/family/models/test_family.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/family/models/test_family.py
@@ -3,10 +3,8 @@ from typing import List
 from django.test import TestCase
 from model_bakery import baker
 
-from .bank_account.bank_account import BankAccount
 from .family import Family
 from .holder.holder import Holder
-from ...baker_recipes import bank_account_recipe
 
 
 class TestFamily(TestCase):
@@ -34,14 +32,12 @@ class TestFamily(TestCase):
         self.assertQuerysetEqual(Family.objects.with_default_holder(), Family.objects.none())
 
     def test_all_families_with_holder_one_family(self):
-        bank_account: BankAccount = baker.make_recipe(bank_account_recipe)
-        holder: Holder = baker.make('Holder', bank_account=bank_account)
+        holder: Holder = baker.make('Holder')
         family: Family = baker.make('Family', default_holder=holder)
         self.assertQuerysetEqual(Family.objects.with_default_holder(), [family])
 
     def test_all_families_with_bank_account_more_than_one_family(self):
-        bank_account: BankAccount = baker.make_recipe(bank_account_recipe)
-        holder: Holder = baker.make('Holder', bank_account=bank_account)
+        holder: Holder = baker.make('Holder')
         families: List[Family] = baker.make('Family', _quantity=3)
         families[0].default_holder = holder
         families[0].save()
