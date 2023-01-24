61359b7f6fb264b406c71bf66cfac187cbc32dd0
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-01-24 10:16:04 +0100

    feat: remove authorization_old

--- software_components/app_server/ampa_members_manager_project/ampa_manager/family/admin/bank_account_admin.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/family/admin/bank_account_admin.py
@@ -1,9 +1,5 @@
-import codecs
-import csv
-
 from django.contrib import admin
 from django.db.models import QuerySet
-from django.http import HttpResponse
 from django.utils.translation import gettext_lazy
 
 from ampa_manager.family.admin.filters.bank_account_filters import BankAccountBICCodeFilter
@@ -17,15 +13,6 @@ class BankAccountAdmin(admin.ModelAdmin):
     list_filter = [BankAccountBICCodeFilter]
     search_fields = ['swift_bic', 'iban']
     list_per_page = 25
-
-    # @admin.action(description=gettext_lazy("Export account owners"))
-    # def export_owners(self, _, bank_accounts: QuerySet[BankAccount]):
-    #     file_name = gettext_lazy('Bank account owners').lower()
-    #     headers = {'Content-Disposition': f'attachment; filename="{file_name}.csv"'}
-    #     response = HttpResponse(content_type='text/csv', headers=headers)
-    #     response.write(codecs.BOM_UTF8)
-    #     csv.writer(response, quoting=csv.QUOTE_ALL).writerows(BankAccount.get_csv_fields(bank_accounts))
-    #     return response
 
     @admin.action(description=gettext_lazy("Complete SWIFT/BIC codes"))
     def complete_swift_bic(self, _, bank_accounts: QuerySet[BankAccount]):
--- software_components/app_server/ampa_members_manager_project/ampa_manager/family/models/bank_account/bank_account.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/family/models/bank_account/bank_account.py
@@ -1,13 +1,11 @@
 from django.core.exceptions import ValidationError
-from django.db import models
-from django.db.models import CASCADE, Manager
+from django.db.models import Manager
 from django.utils.translation import gettext_lazy as _
 from django_extensions.db.models import TimeStampedModel
 from localflavor.generic.models import IBANField, BICField
 
 from ampa_manager.family.models.bank_account.bank_account_queryset import BankAccountQuerySet
 from ampa_manager.family.models.bank_account.bic_code import BicCode
-from ampa_manager.family.models.parent import Parent
 from ampa_manager.family.models.bank_account.iban import IBAN
 
 
@@ -28,13 +26,6 @@ class BankAccount(TimeStampedModel):
 
     def complete_swift_bic(self):
         self.swift_bic = BicCode.get_bic_code(self.iban)
-
-    # @staticmethod
-    # def get_csv_fields(bank_accounts):
-    #     csv_fields = []
-    #     for bank_account in bank_accounts:
-    #         csv_fields.append([str(bank_account.owner), bank_account.iban])
-    #     return csv_fields
 
     def save(self, *args, **kwargs):
         if self.swift_bic in [None, '']:
