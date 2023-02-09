45af4b19bcdbdac7ac2d26f3b9a716838c2d8c8d
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-02-10 00:23:06 +0100

    refactor: clean create_filled from the model

--- software_components/app_server/ampa_members_manager_project/ampa_manager/charge/models/custody/custody_remittance.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/charge/models/custody/custody_remittance.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
-from django.db import models, transaction
-from django.db.models import QuerySet
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from ampa_manager.activity.models.custody.custody_edition import CustodyEdition
 from .custody_remittance_manager import CustodyRemittanceManager
 from ..nameable_with_date import NameableWithDate
-from ...no_custody_edition_error import NoCustodyEditionError
 
 
 class CustodyRemittance(NameableWithDate, models.Model):
@@ -27,13 +25,3 @@ class CustodyRemittance(NameableWithDate, models.Model):
 
     def get_admin_url(self):
         return reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
-
-    @classmethod
-    def create_filled(cls, custody_editions: QuerySet[CustodyEdition]) -> CustodyRemittance:
-        if not custody_editions.exists():
-            raise NoCustodyEditionError
-
-        with transaction.atomic():
-            custody_remittance: CustodyRemittance = CustodyRemittance.objects.create()
-            custody_remittance.custody_editions.set(custody_editions)
-            return custody_remittance
