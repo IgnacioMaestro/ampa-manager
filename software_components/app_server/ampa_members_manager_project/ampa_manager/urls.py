149a20f3b85789434235a8ad90296e7845714c36
Maestro Giron, Ignacio <ignacio.maestro@tecnalia.com>
2023-06-08 10:57:15 +0200

    refactor: convert import_custody view to class

--- software_components/app_server/ampa_members_manager_project/ampa_manager/urls.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from ampa_manager.views.validate_data import validate_families_data
 from ampa_manager.views.check_members import check_members
 from ampa_manager.views.import_after_schools import import_after_schools_registrations, import_after_schools_activities
+from ampa_manager.views.import_custody import ImportCustodyView
 from ampa_manager.views.import_members import import_members
-from ampa_manager.views.import_custody import import_custody
+from ampa_manager.views.validate_data import validate_families_data
 
 urlpatterns = [
     path('members/import/', import_members, name='import_members'),
     path('members/check/', check_members, name='check_members'),
     path('afterschools-registrations/import/', import_after_schools_registrations, name='import_after_schools_registrations'),
     path('afterschools-activities/import/', import_after_schools_activities, name='import_after_schools_activities'),
-    path('custody/import/', import_custody, name='import_custody'),
+    path('custody/import/', ImportCustodyView.as_view(), name='import_custody'),
     path('validations/family/', validate_families_data, name='validate_data'),
 ]
--- software_components/app_server/ampa_members_manager_project/ampa_manager/views/import_custody.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/views/import_custody.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
 
 from ampa_manager.activity.models.custody.custody_edition import CustodyEdition
 from ampa_manager.activity.use_cases.importers.custody_importer import CustodyImporter
@@ -6,47 +8,62 @@ from ampa_manager.forms import ImportCustodyForm
 from ampa_manager.utils.excel.importers_utils import get_excel_columns
 from ampa_manager.views.import_info import ImportInfo
 
-CONTEXT_FIX_PART = {
-    'excel_columns': get_excel_columns(CustodyImporter.COLUMNS_TO_IMPORT),
-    'form_action': '/ampa/custody/import/',
-    'excel_template_file_name': 'templates/plantilla_importar_ludoteca.xls'
-}
 
+class ImportCustodyView(View):
+    TEMPLATE = 'import_custody.html'
 
-def import_custody(request):
-    if request.method == 'POST':
+    @classmethod
+    def post(cls, request):
         form = ImportCustodyForm(request.POST, request.FILES)
         if form.is_valid():
             edition_id = request.POST.get('custody_edition')
             custody_edition = CustodyEdition.objects.get(id=edition_id)
             file_content = request.FILES['file'].read()
             import_info: ImportInfo = CustodyImporter.import_custody(file_content, custody_edition)
-            context = create_full_context(form, import_info)
+            context = cls.__create_context_with_import_info(form, import_info)
         else:
-            context = create_context_only_with_form(form)
-    else:
-        form = ImportCustodyForm()
-        context = create_context_only_with_form(form)
-    return render(request, 'import_custody.html', context)
+            context = cls.__create_context_with_processed_form(form)
+        return render(request, cls.TEMPLATE, context)
 
+    @classmethod
+    def get(cls, request):
+        context = cls.__create_context_with_empty_form()
+        return render(request, cls.TEMPLATE, context)
 
-def create_full_context(form: ImportCustodyForm, import_info: ImportInfo):
-    context = {
-        'form': form,
-        'success': import_info.success(),
-        'import_results': import_info.results,
-        'import_summary': import_info.summary,
-    }
-    context.update(CONTEXT_FIX_PART)
-    return context
+    @classmethod
+    def __create_context_with_import_info(cls, form: ImportCustodyForm, import_info: ImportInfo) -> dict:
+        context = {
+            'form': form,
+            'success': import_info.success(),
+            'import_results': import_info.results,
+            'import_summary': import_info.summary,
+        }
+        context.update(cls.__create_context_fix_part())
+        return context
 
+    @classmethod
+    def __create_context_with_processed_form(cls, form) -> dict:
+        return cls.__create_context_with_form(form)
 
-def create_context_only_with_form(form):
-    context = {
-        'form': form,
-        'success': None,
-        'import_results': None,
-        'import_summary': None,
-    }
-    context.update(CONTEXT_FIX_PART)
-    return context
+    @classmethod
+    def __create_context_with_empty_form(cls) -> dict:
+        return cls.__create_context_with_form(ImportCustodyForm())
+
+    @classmethod
+    def __create_context_with_form(cls, form) -> dict:
+        context = {
+            'form': form,
+            'success': None,
+            'import_results': None,
+            'import_summary': None
+        }
+        context.update(cls.__create_context_fix_part())
+        return context
+
+    @classmethod
+    def __create_context_fix_part(cls) -> dict:
+        return {
+            'excel_columns': get_excel_columns(CustodyImporter.COLUMNS_TO_IMPORT),
+            'form_action': reverse('import_custody'),
+            'excel_template_file_name': 'templates/plantilla_importar_ludoteca.xls'
+        }
