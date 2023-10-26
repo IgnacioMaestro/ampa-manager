ca00479a350b199b4951c74f4e52765562e939d3
Daniel Landa <daniel.landa@tecnalia.com>
2023-10-26 11:51:34 +0200

    search parents by email

--- software_components/app_server/ampa_members_manager_project/ampa_manager/family/admin/parent_admin.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/family/admin/parent_admin.py
@@ -14,7 +14,8 @@ class ParentAdmin(admin.ModelAdmin):
     fields = ['name_and_surnames', 'phone_number', 'additional_phone_number', 'email', 'created', 'modified']
     readonly_fields = ['created', 'modified']
     ordering = ['name_and_surnames']
-    search_fields = ['name_and_surnames', 'family__surnames', 'phone_number', 'additional_phone_number', 'id']
+    search_fields = ['name_and_surnames', 'family__surnames', 'phone_number', 'email', 'additional_phone_number',
+                     'id']
     inlines = [FamilyInline, ReadOnlyHolderInline]
     list_per_page = 25
     list_filter = [ParentFamilyEmailsFilter, ParentFamiliesCountFilter]
