d8fcec4c88b0c5c0edd620e04897f6c6d22888a7
Dani Landa <danilanda@gmail.com>
2023-12-22 18:38:21 +0100

    bat to start server

--- app_ampa.bat
+++ app_ampa.bat
@@ -1,16 +1,16 @@
 @echo off
-title Mantener abierto para usar la app del ampa
+title AMPA. Mantener abierto para usar la aplicacion
 
 :: DEFINIR RUTAS
 set "REPO_PATH=C:\Users\danil\OneDrive\Documents\ampa-manager"
 set "PYTHON_PATH=%REPO_PATH%\software_components\app_server\ampa_members_manager_project\venv\Scripts\python"
 set "DJANGO_MANAGE_PATH=%REPO_PATH%\software_components\app_server\ampa_members_manager_project\manage.py"
 
-echo Accediendo al repositorio
+echo Accediendo a la carpeta de la aplicacion
 cd %REPO_PATH%
 
 echo Actualizando aplicacion
 git pull
 
-echo Iniciando servidor de la aplicacion. NO CIERRES ESTA VENTANA
+echo Iniciando aplicacion (NO CIERRES ESTA VENTANA)
 %PYTHON_PATH% %DJANGO_MANAGE_PATH% runserver 
