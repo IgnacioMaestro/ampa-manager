9759a4a12245df139759156e2f43fa8d4819858c
Daniel Landa <daniel.landa@tecnalia.com>
2024-01-29 15:33:38 +0100

    add remittance dynamic conf fields

--- software_components/app_server/ampa_members_manager_project/ampa_manager/dynamic_settings/admin/dynamic_setting_admin.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/dynamic_settings/admin/dynamic_setting_admin.py
@@ -5,20 +5,20 @@ from django.utils.translation import gettext_lazy as _
 class DynamicSettingAdmin(admin.ModelAdmin):
     list_display = []
     fieldsets = (
-        (None, {
-            'fields': ('ampa_iban',)
+        (_('Remittances'), {
+            'fields': ['remittances_party_id', 'remittances_generic_org_id', 'remittances_bic', 'remittances_iban']
         }),
         (_('Custody'), {
-            'fields': ('custody_members_discount_percent', 'custody_max_days_to_charge_per_month_percent'),
+            'fields': ['custody_members_discount_percent', 'custody_max_days_to_charge_percent'],
         }),
         (_('After-school'), {
-            'fields': (),
+            'fields': [],
         }),
         (_('Camps'), {
-            'fields': (),
+            'fields': [],
         }),
         (_('Members'), {
-            'fields': (),
+            'fields': [],
         }),
     )
     readonly_fields = []
--- software_components/app_server/ampa_members_manager_project/ampa_manager/dynamic_settings/dynamic_settings.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/dynamic_settings/dynamic_settings.py
@@ -6,6 +6,14 @@ from ampa_manager.dynamic_settings.singleton import Singleton
 
 
 class DynamicSetting(Singleton):
-    ampa_iban = IBANField(unique=True, verbose_name=_("IBAN"))
-    custody_members_discount_percent = models.FloatField(verbose_name=_("Members discount percent"))
-    custody_max_days_to_charge_per_month_percent = models.IntegerField(verbose_name=_("Max days percent to charge each month"))
+    remittances_party_id = models.CharField(max_length=500, verbose_name=_("Party identification"))
+    remittances_generic_org_id = models.CharField(max_length=500, verbose_name=_("Generic organisation identification"))
+    remittances_bic = models.CharField(max_length=500, verbose_name=_("BIC"))
+    remittances_iban = IBANField(unique=True, verbose_name=_("IBAN"))
+
+    custody_members_discount_percent = models.FloatField(verbose_name=_("Members discount"),
+                                                         help_text=_('Percentage discounted to members in the price of '
+                                                                     'the custody'))
+    custody_max_days_to_charge_percent = models.IntegerField(verbose_name=_("Max days to charge"),
+                                                             help_text=_('Maximum number of days in a month (as a '
+                                                                         'percentage) to charge to each user'))
