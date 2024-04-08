1470a0396bba1ec49228e64f9563c716e8ec0547
Daniel Landa <daniel.landa@tecnalia.com>
2024-04-08 10:48:47 +0200

    add missing import

--- software_components/app_server/ampa_members_manager_project/ampa_manager/activity/admin/custody_admin.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/activity/admin/custody_admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import admin
 from django.db.models import QuerySet
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _, gettext_lazy
 
