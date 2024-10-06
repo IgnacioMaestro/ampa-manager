542669de228059c945764d7f4f278791a6aedb70
Daniel Landa <daniel.landa@tecnalia.com>
2024-10-06 16:16:30 +0200

    refactor template

--- software_components/app_server/ampa_members_manager_project/ampa_manager/views/import_custody.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/views/import_custody.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from django.db import transaction
 from django.shortcuts import render
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django.views import View
 
 from ampa_manager.activity.models.custody.custody_edition import CustodyEdition
@@ -17,19 +19,30 @@ class SimulationException(Exception):
 class ImportCustody(View):
     HTML_TEMPLATE = 'import_custody.html'
     EXCEL_TEMPLATE = 'templates/plantilla_importar_ludoteca.xls'
+    IMPORTER_TITLE = _('Import custody')
+    VIEW_NAME = 'import_custody'
+
+    @classmethod
+    def get_context(cls, form: Optional[ImportCustodyForm] = None) -> dict:
+        if not form:
+            form = ImportCustodyForm()
+
+        return {
+            'form': form,
+            'importer_title': cls.IMPORTER_TITLE,
+            'view_url': reverse(cls.VIEW_NAME),
+            'excel_columns': CustodyImporter.get_excel_columns(CustodyImporter.COLUMNS_TO_IMPORT),
+            'excel_template_file_name': cls.EXCEL_TEMPLATE
+        }
 
     @classmethod
     def get(cls, request):
-        context = {}
-        context['form'] = ImportCustodyForm()
-        context['excel_columns'] = CustodyImporter.get_excel_columns(CustodyImporter.COLUMNS_TO_IMPORT)
-        context['excel_template_file_name'] = cls.EXCEL_TEMPLATE
-        return render(request, cls.HTML_TEMPLATE, context)
+        return render(request, cls.HTML_TEMPLATE, cls.get_context())
 
     @classmethod
     def post(cls, request):
-        context = {}
         form = ImportCustodyForm(request.POST, request.FILES)
+        context = cls.get_context(form)
 
         if form.is_valid():
             result: ImportExcelResult = cls.import_custody(
@@ -38,15 +51,12 @@ class ImportCustody(View):
                 simulation=request.POST.get('simulation')
             )
 
-            context['result'] = {}
-            context['result']['success'] = result.success
-            context['result']['rows'] = result.rows
+            context['result'] = {
+                'success': result.success,
+                'rows': result.rows
+            }
 
-        context['form'] = form
-        context['excel_columns'] = CustodyImporter.get_excel_columns(CustodyImporter.COLUMNS_TO_IMPORT)
-        context['excel_template_file_name'] = cls.EXCEL_TEMPLATE
         context['simulation'] = request.POST.get('simulation')
-
         return render(request, cls.HTML_TEMPLATE, context)
 
     @classmethod
