dd4a1ada8011a0ebb25bdf84bffbf72909fa66a0
107596 <daniel.landa@tecnalia.com>
2025-10-10 14:21:38 +0200

    update phrasing in templates for clearer instructions and integrate custom admin menu in membership campaign notification view

--- software_components/app_server/ampa_members_manager_project/ampa_manager/views/notifiers/notify_membership_campaign_view.py
+++ software_components/app_server/ampa_members_manager_project/ampa_manager/views/notifiers/notify_membership_campaign_view.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 from django.views import View
 
 from ampa_manager.charge.use_cases.membership.membership_campaign_notifier import MembershipCampaignNotifier
-from ampa_manager.charge.use_cases.membership.families_notifier_result import FamiliesNotifierResult
 from ampa_manager.family.models.family import Family
+from ampa_manager_project.admin import CustomAdminSite
 
 
 class NotifyMembershipCampaignView(View):
@@ -21,6 +21,7 @@ class NotifyMembershipCampaignView(View):
             'families_renew_url': f'{family_changelist_url}?member=renew',
             'families_not_renew_out_of_school_url': f'{family_changelist_url}?member=no_renew_no_school_children',
             'families_not_renew_declined_url': f'{family_changelist_url}?member=no_renew_declined',
+            'menu': CustomAdminSite.get_custom_app_list()
         }
 
     @classmethod
